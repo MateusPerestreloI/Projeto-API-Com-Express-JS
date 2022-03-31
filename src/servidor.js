21a0766c026337b879ed9bb8b43b669a72bc82c2
Mateus I <mateuspi.0398@gmail.com>
2022-03-31 16:34:08 -0300

    Correção de erro - CRUD
    Correção de erro ao executar o CRUD, e adição dos métodos "put" e "delete"

--- src/servidor.js
+++ src/servidor.js
@@ -1,9 +1,11 @@
 const porta = 3003
 
 const express = require('express')
-const req = require('express/lib/request')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos()) // Converter para JSON
@@ -16,9 +18,23 @@ app.get('/produtos/:id', (req, res, next) => {
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
         nome: req.body.nome,
-        preoc: req.body.preco
+        preco: req.body.preco
     })
     res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) 
 })
 
 app.listen(porta, () => {
