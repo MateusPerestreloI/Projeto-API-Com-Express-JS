4c564e6307b059241b7df9830b8422425f2aa068
Mateus I <mateuspi.0398@gmail.com>
2022-03-31 16:22:46 -0300

    Banco de Dados
    Implementação do banco de dados

--- src/servidor.js
+++ src/servidor.js
@@ -1,10 +1,24 @@
 const porta = 3003
 
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', (request, response, next) => {
-    response.send({ nome: 'Notebook', preco: 123.45 }) // Converter para JSON
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos()) // Converter para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preoc: req.body.preco
+    })
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
